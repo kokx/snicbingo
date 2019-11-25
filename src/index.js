b1cce923277aca9146049e948ac0ce53535a9d5a
Pieter Kokx <pieter@kokx.org>
2019-11-25 18:19:15 +0100

    Added initial creation endpoint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.post('/create', (req, res) => {
+    console.log(req.body);
+
+    res.send("Test");
+});
 
 app.use('/', express.static('public/', { maxAge: 3600*1000 }));
 
